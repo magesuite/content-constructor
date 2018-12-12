6b20920c3e0daba491f772b09314ac527fe69b74
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2018-12-12 07:35:28 +0100

    Updated tests and twig

--- src/Components/Paragraph/ParagraphAdmin.php
+++ src/Components/Paragraph/ParagraphAdmin.php
@@ -14,20 +14,15 @@ class ParagraphAdmin extends \MageSuite\ContentConstructor\AbstractAdminComponen
      */
     private $locator;
 
-    /**
-     * @var WysiwygConfigDataProvider
-     */
-    private $wysiwygConfigDataProvider;
 
     public function __construct(
         \MageSuite\ContentConstructor\View\Template $template,
-        \MageSuite\ContentConstructor\View\AdminTemplateLocator $locator,
-        WysiwygConfigDataProvider $wysiwygConfigDataProvider
+        \MageSuite\ContentConstructor\View\AdminTemplateLocator $locator
     )
     {
         $this->template = $template;
         $this->locator = $locator;
-        $this->wysiwygConfigDataProvider = $wysiwygConfigDataProvider;
+
     }
 
     public function renderConfigurator()
@@ -36,7 +31,7 @@ class ParagraphAdmin extends \MageSuite\ContentConstructor\AbstractAdminComponen
             $this->locator->locate('paragraph/configurator/paragraph.twig'),
             [
                 'configuration' => $this->getComponentConfiguration(),
-                'wysiwyg_configuration' => $this->wysiwygConfigDataProvider->getConfig()
+                'wysiwyg_configuration' => new \Magento\Cms\Model\WysiwygDefaultConfig()
             ]
         );
     }
